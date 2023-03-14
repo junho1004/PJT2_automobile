5f9990ae0dbae22cfebcac60dc809f60e9101d42
DohyeonYeo <ydh51216@gmail.com>
2023-03-15 02:42:33 +0900

    [FE] feature : kakao map api
    Former-commit-id: e4f1a703f3ec45f53623cbe1ab61d1641219c341

--- 와따렌트/frontend/src/pages/Map/GpsPage.jsx
+++ 와따렌트/frontend/src/pages/Map/GpsPage.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import styles from "./GpsPage.module.css";
 
+const { kakao } = window
+
 export default function GpsPage() {
   const navigate = useNavigate();
+  useEffect (() => {
+    let container = document.getElementById('map')
+    let options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3
+    }
+    let map = new kakao.maps.Map(container, options)
+  })
 
   return (
-    <div className={styles.background}>
-        <div style={{padding:"50px"}}>
-          <div className={styles.texta}>하이요</div>
+    <div className = {styles.background}>
+        <div style = {{padding:"50px"}}>
+          <div 
+            id = "map"
+            className = {styles.texta} 
+            style = {{
+              width: "240px",
+              height: "400px"
+            }}>ㅇㅇㅇ</div>
           </div>
     </div>
   );
