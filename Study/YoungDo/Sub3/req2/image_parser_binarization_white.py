bef3aba5f46e1c8fd9000695f09e434a0d91921e
asd200df <109517068+asd200df@users.noreply.github.com>
2023-03-23 17:45:45 +0900

    [EMB] Study : youngdo
    Camera image parser 1

---
+++ Study/YoungDo/Sub3/req2/image_parser_binarization_white.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import rospy
+import cv2
+import numpy as np
+import os, rospkg
+
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridgeError
+
+# image parser binarization Node 는 시뮬레이터에서 송신하는 Camera 센서 정보를 받아 실시간으로 출력하는 예제입니다.
+# 출력시 hsv 특정 영역의 색상 범위를 지정하여 원하는 색상의 영역만 특정하여 출력합니다.
+
+# 노드 실행 순서 
+# 1. HSV 색상 영역 지정
+# 2. 특정 영역의 색상 검출
+# 3. 이미지 출력
+
+class IMGParser:
+    def __init__(self):
+
+        self.image_sub = rospy.Subscriber("/image_jpeg/compressed", CompressedImage, self.callback)
+
+    def callback(self, msg):
+        try:
+            
+            np_arr = np.fromstring(msg.data, np.uint8)
+            img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            
+        except CvBridgeError as e:
+            print(e)
+        
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+
+        
+        #TODO: (1)
+        
+        # 특정 색상 영역을 검출하기 위해 범위를 지정합니다.
+        # 하한 값 행렬과 상한 값 행렬을 정해 그 사이의 값 만을 출력 하도록 합니다.
+        # 이번 예제에서는 흰색 영역을 검출합니다.
+
+        lower_wlane = np.array([   0 ,    0   ,     200  ])
+        upper_wlane = np.array([  180  ,    30   ,   255    ])
+
+        
+
+        #TODO: (2)
+        
+        # cv2.inRange 함수는 특정 색상 영역을 추출할 수 있습니다. 
+        # cv2.inRange 함수를 이용하여 HSV 이미지에서 색상 범위를 지정합니다.
+        # 함수의 첫번째 변수에는 이미지 정보를 두번째는 하한 값 세번째는 상한 값 행렬식을 넣습니다.
+
+        img_wlane = cv2.inRange(img_hsv, lower_wlane, upper_wlane)
+
+        
+        # img_wlane = cv2.cvtColor(img_wlane, cv2.COLOR_HSV2BGR)
+
+        # i cant concatenate 3 color cause array's size
+        img_concat= np.concatenate((img_bgr, img_hsv), axis = 1)
+
+        #print(img_bgr.shape)
+        #print(img_hsv.shape)
+        #print(img_wlane.shape)
+
+        #TODO: (3)
+        
+        # 이미지를 출력 합니다.
+        cv2.imshow("bgr_hsv", img_concat)
+        cv2.imshow("wlane", img_wlane)
+        cv2.waitKey(1) 
+        
+        
+
+if __name__ == '__main__':
+
+    rospy.init_node('image_parser', anonymous=True)
+
+    image_parser = IMGParser()
+
+    rospy.spin() 
