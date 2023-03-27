2a3d62bcf3396a76e38a12233fb5461a112dc16b
여도현 <ydh51216@gmail.com>
2023-03-28 02:31:10 +0900

    [FE] feature : login & signup & calendar & timepicker

--- 와따렌트/backend/src/main/java/com/isf6/backend/domain/repository/LiveBidRepostiory.java
+++
@@ -1,7 +0,0 @@
-package com.isf6.backend.domain.repository;
-
-import com.isf6.backend.domain.entity.LiveBid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LiveBidRepostiory extends JpaRepository<LiveBid, Long> {
-}
